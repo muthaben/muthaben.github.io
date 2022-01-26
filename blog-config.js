7989da77498ea3816682c3eb9192c2d17819229c
muthaben <senarkimi@gmail.com>
2022-01-26 12:01:43 +0900

    initial commit

---
+++ blog-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  title: "Muthaben's Library",
+  description: "하려면 방법을 찾고 피하려면 핑계를 찾는다",
+  author: "Muthaben",
+  siteUrl: "https://muthaben.github.io",
+  links: {
+    github: "https://github.com/muthaben",
+    // linkedIn: "https://linkedin.com",
+    // facebook: "https://www.facebook.com",
+    // instagram: "https://www.instagram.com",
+    email: "wskim.dev@gmail.com",
+  },
+  utterances: {
+    repo: "muthaben/muthaben.github.io", // "devHudi/gatsby-starter-hoodie"
+    type: "pathname",
+  },
+}
