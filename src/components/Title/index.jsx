cc1a907bf42bca6c4608ec9cef759b3166688b33
muthaben <senarkimi@gmail.com>
2022-01-27 00:38:05 +0900

    Edit heading size in Title component

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -22,9 +22,9 @@ const Wrapper = styled.h1`
 
 const Title = ({ size, children }) => {
   const sizes = {
-    sm: "19.2px",
-    md: "25.6px",
-    bg: "33.6px",
+    sm: "12px",
+    md: "18px",
+    bg: "24px",
   }
 
   return <Wrapper size={sizes[size]}> {children} </Wrapper>
